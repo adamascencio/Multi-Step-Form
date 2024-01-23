6172b4de737968a7169fb4525bf3813b584b08d5
adamascencio <ascencioadam@gmail.com>
2024-01-22 19:53:50 -0800

    Add event listeners to all next buttons. Add event listener to "Go Back" link to allow user to go back to a previous step of the form.

--- main.js
+++ main.js
@@ -9,16 +9,22 @@ const step1Sidebar = document.getElementById('step1');
 const step2Sidebar = document.getElementById('step2');
 const step3Sidebar = document.getElementById('step3');
 const step4Sidebar = document.getElementById('step4');
-const nextStepBtn = document.getElementsByClassName('next-btn')[0];
 const personalInfoEl = document.getElementById('personal-info');
 const selectPlanEl = document.getElementById('select-plan');
 const addOnsEl = document.getElementById('add-ons');
 const confirmationEl = document.getElementById('confirmation');
+const allNextButtons = document.getElementsByClassName('next-btn');
+const allBackLinks = document.getElementsByClassName('back');
 
 /*----- event listeners -----*/
-/* listen for Next Step button click */
-nextStepBtn.addEventListener('click', handleNextClick);
+// add event listener to all next buttons
+for (let i = 0; i < allNextButtons.length; i++) {
+  allNextButtons[i].addEventListener('click', handleNextClick);
+}
 
+for (let i = 0; i < allBackLinks.length; i++) {
+  allBackLinks[i].addEventListener('click', handleBackClick);
+}
 
 /*----- functions -----*/
 function handleNextClick(evt) {
@@ -28,6 +34,18 @@ function handleNextClick(evt) {
   stepSidebarArr[currentStep].classList.remove('step-selected');
   stepContentArr[currentStep].classList.add('hidden');
   currentStep++;
+  stepSidebarArr[currentStep].classList.add('step-selected');
+  stepContentArr[currentStep].classList.remove('hidden');
+  console.log('clicked')
+}
+
+function handleBackClick(evt) {
+  evt.preventDefault();
+  const stepSidebarArr = [step1Sidebar, step2Sidebar, step3Sidebar, step4Sidebar];
+  const stepContentArr = [personalInfoEl, selectPlanEl, addOnsEl, confirmationEl];
+  stepSidebarArr[currentStep].classList.remove('step-selected');
+  stepContentArr[currentStep].classList.add('hidden');
+  currentStep--;
   stepSidebarArr[currentStep].classList.add('step-selected');
   stepContentArr[currentStep].classList.remove('hidden');
   console.log('clicked')
